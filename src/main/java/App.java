19b3753728d212fe2a6db1a8beb37ba508552634
Meghaab04 <meghaagowda16@gmail.com>
2026-04-11 14:49:30 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Dummy Java App Running...");
-
-        // Simulate some work
-        for (int i = 1; i <= 3; i++) {
-            System.out.println("Step " + i + " completed");
-        }
-
-        System.out.println("Build Successful!");
+        System.out.println("Hello Jenkins!");
     }
 }
