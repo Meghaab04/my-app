eb46c1f508460fea80b0d4d450e1d2078b753575
Meghaab04 <meghaagowda16@gmail.com>
2026-04-11 13:13:06 +0530

    Create App.java

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Dummy Java App Running...");
+
+        // Simulate some work
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Step " + i + " completed");
+        }
+
+        System.out.println("Build Successful!");
+    }
+}
